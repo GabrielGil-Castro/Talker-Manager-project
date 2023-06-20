51a42d57ab8b3312c41efd1e3eb4c662b5d463f6
GabrielGil-Castro <gsgc05@gmail.com>
2023-06-20 11:05:00 -0300

    Requisito 3 completo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const crypto = require('crypto');
+const { readFile } = require('./utils/readFiles');
 
 const app = express();
 app.use(express.json());
@@ -15,4 +17,25 @@ app.listen(PORT, () => {
   console.log('Online');
 });
 
-//iniciando projeto 
+app.get('/talker', async (req, res) => {
+  const talkers = await readFile();
+  if (talkers === []) return res.status(HTTP_OK_STATUS).json([]);
+  return res.status(HTTP_OK_STATUS).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await readFile();
+  const { id } = req.params;
+  const findTalkerById = talkers.find((talker) => talker.id === Number(id));
+  if (!findTalkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+    
+  return res.status(HTTP_OK_STATUS).json(findTalkerById);
+});
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const token = crypto.randomBytes(8).toString('hex');
+  if (email && password) return res.status(HTTP_OK_STATUS).json({ token });
+});
